c2ba47ba97a545d8dd3ebd0fe5481c7e31f32e0f
maxon <obaogeayobami@gmail.com>
2024-05-02 01:00:14 +0100

    navigation coming up gradually

--- HintAll website/hintall.js
+++ HintAll website/hintall.js
@@ -3,28 +3,55 @@ var navContainer = document.querySelector(".navContainer")
 var openNavContainer = document.querySelector(".openNavContainer");
 var hintAllName = document.querySelector(".hintAllName")
 var logoNameHolder = document.querySelector(".logoNameHolder")
+var navUnderling = document.querySelector('.underling')
+var pageLink = document.querySelectorAll
+(".pageLinkHolder a")
 
 //eventListeners
 openNavContainer.addEventListener("click",openNavigation);
+
+//for each
+
 
 //functions
 
 function openNavigation() {
   var navContainerWidth = navContainer.getBoundingClientRect().width;
   if (navContainerWidth == 70) {
-    navContainer.classList.add('newNavWidth')
-    openNavContainer.innerHTML =`<i class="fa-solid fa-xmark"></i>`
-    hintAllName.classList.remove('fadeOutText')
-    hintAllName.classList.add('hintAllNameDesign')
-    logoNameHolder.classList.add('logoNameHolderFlex')
+    addWidthAndDesing()
   } else {
-    navContainer.classList.remove('newNavWidth')
-    openNavContainer.innerHTML =`<i class="fa-solid fa-bars"></i>`
-    hintAllName.classList.add('fadeOutText')
-    hintAllName.classList.remove('hintAllNameDesign')
+    removeWidthAndDesing()
   }
 }
-function 
-designHintAllLogoNameAndNavigations(){
-  
+function addWidthAndDesing() {
+  navContainer.classList.add('newNavWidth')
+openNavContainer.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+hintAllName.classList.remove('fadeOutText')
+hintAllName.classList.add('hintAllNameDesign')
+hintAllName.classList.remove('hintAllName')
+navUnderling.classList.add
+('newUnderlingDesing')
+pageLink.forEach(addDesingToNavLinks)
+}
+
+function removeWidthAndDesing() {
+navContainer.classList.remove('newNavWidth')
+openNavContainer.innerHTML = `<i class="fa-solid fa-bars"></i>`
+hintAllName.classList.add('fadeOutText')
+hintAllName.classList.remove('hintAllNameDesign')
+hintAllName.classList.add('hintAllName')
+navUnderling.classList.remove('newUnderlingDesing')
+pageLink.forEach(removeNavDesings)
+
+} 
+
+function addDesingToNavLinks(links) {
+  links.classList.add('fadeInAnchorLinks')
+  links.classList.remove('navLinks')
+  links.classList.remove('fadeOutAnchorLinks')
+}
+function removeNavDesings(links) {
+  links.classList.add('fadeOutAnchorLinks')
+links.classList.add('navLinks')
+links.classList.remove('fadeinAnchorLinks')
 }
